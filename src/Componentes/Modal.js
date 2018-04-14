4a039705b7dbbf6c07e0bcbf933dcc6602643f33
adriangnz <aegg2000@gmail.com>
2018-04-14 14:13:49 -0400

    Añadido los campos para cambios de indentificador y descipcion

--- src/Componentes/Modal.js
+++ src/Componentes/Modal.js
@@ -3,6 +3,21 @@ import {Modal, Button} from 'react-bootstrap';
 
 
 class MyLargeModal extends React.Component {
+    state ={
+      nombre : this.props.camara.nombre,
+      lugar : this.props.camara.lugar
+    };
+
+    change = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+      
+      this.props.camara.nombre = this.state.nombre,
+      this.props.camara.lugar = this.state.lugar
+   
+    };
+
     render() {
       return (
         <Modal
@@ -11,12 +26,26 @@ class MyLargeModal extends React.Component {
           aria-labelledby="contained-modal-title-lg"
         >
           <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-lg" className="text-center">{this.props.camara.nombre}: {this.props.camara.lugar}</Modal.Title>
+            <Modal.Title id="contained-modal-title-lg" className="text-center">{this.state.nombre}: {this.state.lugar}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <img src={this.props.camara.src} alt={this.props.camara.nombre} className="Fuente"></img>
           </Modal.Body>
           <Modal.Footer>
+   
+                <label>Identificador:</label>
+                <input
+                    name="nombre"
+                    placeholder="First name"
+                    onChange={e => this.change(e)}
+                  />
+                <label>Descripcion:</label>
+                <input
+                    name="lugar"
+                    placeholder="lugar"
+                    onChange={e => this.change(e)}
+                  />
+       
             <Button onClick={this.props.onHide}>Close</Button>
           </Modal.Footer>
         </Modal>
